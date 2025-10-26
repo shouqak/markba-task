beaf32ef87813fcdcebcf95815fe0660042860db
shouq <shouqbis@gmail.com>
2025-10-26 23:11:29 +0300

    add vercel josn

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist'
+  },
+  base: '/',
+  server: {
+    port: 5173,
+    open: true
+  }
 })
